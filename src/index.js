8400d52557de2c69e982d160b80ec16c4434a438
CitlaliSC <citlaserratos22@gmail.com>
2025-09-22 22:24:22 -0600

    feat(api): add query creation and routes

--- src/index.js
+++ src/index.js
@@ -5,6 +5,7 @@ import cors from 'cors';
 import Response from './domain/response.js';
 import logger from './util/logger.js';
 import HttpStatus from './controller/patient.controller.js';
+import patientRoutes from './route/patient.route.js';
 
 dotenv.config();
 const PORT = process.env.SERVER_PORT || 3000;
@@ -12,5 +13,11 @@ const app = express();
 app.use(cors({ origin: '*' })); //allow any app to call this app, change later on
 app.use(express.json()); // get response on json format
 
+app.use('/patients', patientRoutes);
 app.get('/', (req, res) => res.send( new Response(HttpStatus.OK.code, HttpStatus.OK.status, 'System up') ));
+app.use((req, res) => {
+    res.status(HttpStatus.NOT_FOUND.code).send(
+      new Response(HttpStatus.NOT_FOUND.code, HttpStatus.NOT_FOUND.status, 'Route does not exist on the server')
+    );
+});
 app.listen(PORT, ()=> logger.info(`Server running on: ${ip.address()}:${PORT}`));
