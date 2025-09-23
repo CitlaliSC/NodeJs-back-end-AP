cc14b74d25d2409ea1800ee242ab12f6f567e8a9
CitlaliSC <citlaserratos22@gmail.com>
2025-09-22 18:11:39 -0600

    chore(db): init MySQL db & connection

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ import express from 'express';
 import ip from 'ip';
 import dotenv from 'dotenv';
 import cors from 'cors';
+import Response from '../src/domain/response.js';
 
 dotenv.config();
 const PORT = process.env.SERVER_PORT || 3000;
@@ -9,6 +10,5 @@ const app = express();
 app.use(cors({ origin: '*' })); //allow any app to call this app, change later on
 app.use(express.json()); // get response on json format
 
-app.get('/', (req, res) => { res.send( { message: 'UP' }) });
-console.log(process.env);
+app.get('/', (req, res) => res.send( new Response(200, 'OK', 'System up') ));
 app.listen(PORT, ()=> console.log(`Server running on: ${ip.address()}:${PORT}`));
